4dcbfb45afe544f3843ff7eca83a27760a54ce29
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-07 23:53:31 +0600

    cart calculation implimented on order details component

--- src/Components/Order-details/OrderDetails.js
+++ src/Components/Order-details/OrderDetails.js
@@ -5,8 +5,17 @@ import auth from "../../firebase.init";
 
 const OrderDetails = () => {
   const [user] = useAuthState(auth);
-  const [quantity, setQuantity] = useState(0);
   const [items, setItems] = useState([]);
+
+  let totalItem = 0;
+  let totalFee = 0;
+  for (const item of items) {
+    totalItem += item.quantity;
+    totalFee += parseFloat(item.price) * parseInt(item.quantity);
+    console.log(item);
+  }
+  const tax = totalFee * 0.1;
+  const grandTotal = totalFee + tax;
 
   useEffect(() => {
     fetch(`http://localhost:5000/cart?email=${user.email}`)
@@ -14,17 +23,10 @@ const OrderDetails = () => {
       .then((data) => setItems(data));
   }, [user]);
 
-  const handleIncrease = () => {
-    console.log("inc");
-  };
-  const handleDecrease = () => {
-    console.log("dec");
-  };
-
   return (
     <div className="order-details-container container">
       <div className="order-details">
-        {/* <div className="delivery-details">
+        <div className="delivery-details">
           <h2>Edit Delivery Details</h2>
           <input type="text" name="" id="" placeholder="Deliver to door" />
           <br />
@@ -42,8 +44,8 @@ const OrderDetails = () => {
           />
           <br />
           <input type="submit" value="Save & Continue" />
-        </div> */}
-        <div style={{ width: "500px" }} className="cart-items-container">
+        </div>
+        <div style={{ minWidth: "350px" }} className="cart-items-container">
           <div className="info">
             <p>
               From <span>Fulshan Plaza Restura GPR</span>
@@ -57,32 +59,32 @@ const OrderDetails = () => {
                 <img src={item.img} alt="" />
                 <div className="cart-item-info">
                   <p>{item.name}</p>
-                  <h4>$ {item.price}</h4>
+                  <h4>
+                    $ {item.price}{" "}
+                    <div className="d-inline text-secondary">
+                      x <small>{item.quantity}</small>
+                    </div>
+                  </h4>
                   <span>Delivery free</span>
-                </div>
-                <div className="qnty">
-                  <span onClick={handleDecrease}>-</span>
-                  <span>{item.quantity}</span>
-                  <span onClick={handleIncrease}>+</span>
                 </div>
               </div>
             ))}
           </div>
           <div className="receipt">
             <p>
-              Total Item: <span>$129</span>
+              Total Item: <span>{totalItem}</span>
             </p>
             <p>
-              Subtotal: <span>$129</span>
+              Subtotal: <span>${totalFee}</span>
             </p>
             <p>
-              Tax: <span>$129</span>
+              Tax: <span>${tax}</span>
             </p>
-            <p>
+            {/* <p>
               Delivery Fee: <span>$0</span>
-            </p>
+            </p> */}
             <h4>
-              Total: <span>$129</span>
+              Total: <span>${grandTotal.toFixed(2)}</span>
             </h4>
             <button>Place Order</button>
           </div>
